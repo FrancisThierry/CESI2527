332766d3a70bcb35d7bf853691421f2a254842a1
FrancisThierry <boulangerthie@gmail.com>
2025-12-08 13:31:41 +0100

    ajout proxy pour css

--- server/app.js
+++ server/app.js
@@ -1,8 +1,24 @@
 const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
+    // Reverse proxy pour les ressources externes
+    if (req.url === '/weweb.css') {
+        const proxyUrl = 'http://172.26.152.177/weweb.css';
+        
+        http.get(proxyUrl, (proxyRes) => {
+            res.writeHead(proxyRes.statusCode, proxyRes.headers);
+            proxyRes.pipe(res);
+        }).on('error', (err) => {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Erreur proxy');
+        });
+        return;
+    }
+    
+    // Servir index.html par défaut
     const filePath = path.join(__dirname, 'index.html');
     
     fs.readFile(filePath, 'utf8', (err, data) => {
@@ -17,7 +33,7 @@ const server = http.createServer((req, res) => {
     });
 });
 
-const PORT = 3000;
+const PORT = 3001;
 server.listen(PORT, () => {
     console.log(`Serveur en écoute sur http://localhost:${PORT}`);
 });
